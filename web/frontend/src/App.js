6e8511d41163eb86fce271e99f1df33c8075d976
AbelTruong <abeltruong.arena@gmail.com>
2023-04-08 21:34:20 +0700

    run frontend reactjs

--- web/frontend/src/App.js
+++ web/frontend/src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import PropTypes from 'prop-types'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+App.propTypes = {}
+
+function App(props) {
+  return <div>Xin chao chung may nha!!</div>
 }
 
-export default App;
+export default App
