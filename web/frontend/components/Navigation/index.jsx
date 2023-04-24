05e25bccfac87ae27bd0613ac93a7c7d9955f6d8
AbelTruong <abeltruong.arena@gmail.com>
2023-04-24 22:15:18 +0700

    update code responsive navigation 20232404

--- web/frontend/components/Navigation/index.jsx
+++ web/frontend/components/Navigation/index.jsx
@@ -1,14 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import LightDarkMode from '../LightDarkMode'
 import { Link } from 'react-router-dom'
 
 const Navigation = (props) => {
+  console.log('props :>> ', props)
   const { primaryActions } = props
+
+  const [showMenubars, setShowMenuBars] = useState(false)
+
+  console.log('🚀🚀🚀 ~ file: index.jsx:13 ~ Navigation ~ showMenubars:', showMenubars)
 
   return (
     <nav className="px-2 sm:px-4 py-2.5 border-b-2">
       <div className="flex flex-wrap items-center justify-between">
+        <div
+          className="w-10 h-10 md:hidden text-te-light dark:text-te-dark"
+          type="button"
+          onClick={() => setShowMenuBars(!showMenubars)}
+        >
+          {showMenubars ? (
+            <svg
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="1.5"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+              />
+            </svg>
+          ) : (
+            <svg
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="1.5"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M19.5 5.25l-7.5 7.5-7.5-7.5m15 6l-7.5 7.5-7.5-7.5"
+              />
+            </svg>
+          )}
+        </div>
         <div className="text-green-900 hover:text-te-light dark:text-te-dark m-2 font-bold text-[32px]">
           <Link to="/">APT2</Link>
         </div>
