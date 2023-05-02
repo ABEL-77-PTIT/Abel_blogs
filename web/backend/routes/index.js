77ebbaee058915c894c744e90469f7231be64d1d
AbelTruong <abeltruong.arena@gmail.com>
2023-05-03 00:57:42 +0700

    add table use in db

--- web/backend/routes/index.js
+++ web/backend/routes/index.js
@@ -1,5 +1,9 @@
 import ProductRoutes from './product.js'
+import UserRoutes from './user.js'
+import AuthRoutes from './auth.js'
 
 export default function adminRoute(app) {
   ProductRoutes(app)
+  UserRoutes(app)
+  AuthRoutes(app)
 }
