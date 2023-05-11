919874cdacea8872c854fbacd90f8c1eb0093470
AbelTruong <abeltruong.arena@gmail.com>
2023-05-11 16:54:41 +0700

    clear log

--- web/index.js
+++ web/index.js
@@ -26,15 +26,6 @@ const NODE_ENV = process.env.NODE_ENV || 'development'
 // create express app
 const app = express()
 
-console.log('log lai cai cho dui')
-console.log('them cai cho dui')
-console.log('log lai cai cho dui')
-console.log('them cai cho dui')
-console.log('log lai cai cho dui')
-console.log('them cai cho dui')
-console.log('log lai cai cho dui')
-console.log('them cai cho dui')
-
 /**
  * app.set(): thiết lập một số cấu hình cho ứng dụng
  * Ví dụ như biến môi trường, port, tệp dẫn tĩnh hoặc Views engine và bla bla...
